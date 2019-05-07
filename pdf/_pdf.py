51319462cc596407055e04cab0356ce83739b4c5
Dan <dansonlinepresence@gmail.com>
2019-05-07 13:59:45 -0400

    split out to_bytes

--- pdf/_pdf.py
+++ pdf/_pdf.py
@@ -1,67 +1,9 @@
 from ._parser import Parser
 from ._objects import Name, Ref, Stream
+from ._to_bytes import to_bytes
 
 import copy
 import pprint
-import re
-import string
-
-def transform_typical(x):
-    return repr(x)
-
-def transform_ref(x):
-    return '{} R'.format(x)
-
-def transform_string(x):
-    if any(i not in string.printable for i in x):
-        x = ''.join('{:02X}'.format(ord(i)) for i in x)
-        return '<{}>'.format(x)
-    else:
-        x = re.sub(r'([()\\])', r'\\\1', x)
-        return '({})'.format(x)
-
-def transform_dictionary(x):
-    result = b'<<'
-    for k, v in x.items():
-        result += transform_typical(Name(k)).encode('utf-8') + b' ' + to_bytes(v) + b' '
-    result += b'>>'
-    return result
-
-def transform_stream(x):
-    result = transform_dictionary(x.dictionary)
-    result += b'\nstream\n'
-    result += x.stream
-    result += b'\nendstream'
-    return result
-
-def transform_array(x):
-    result = b'['
-    for i in x:
-        result += to_bytes(i) + b' '
-    result += b']'
-    return result
-
-def transform_bool(x):
-    return {True: 'true', False: 'false'}[x]
-
-def transform_null(x):
-    return 'null'
-
-def to_bytes(object):
-    result = {
-        Name: transform_typical,
-        Ref: transform_ref,
-        int: transform_typical,
-        float: transform_typical,
-        str: transform_string,
-        dict: transform_dictionary,
-        Stream: transform_stream,
-        list: transform_array,
-        bool: transform_bool,
-        None: transform_null,
-    }[object.__class__](object)
-    if type(result) == str: result = result.encode('utf-8')
-    return result
 
 class Xref:
     def __init__(self, offset, generation_number, keyword):
